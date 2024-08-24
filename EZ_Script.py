6fb86080b2c47dc294e34fe6e33ab35e30442600
Abbbbb <144189002+Abbbbbqvq@users.noreply.github.com>
2024-08-24 11:42:08 +0800

    Update EZ_Script.py

--- EZ_Script.py
+++ EZ_Script.py
@@ -2,10 +2,12 @@ import subprocess
 import re
 import requests
 
+black_list = ["网站防火墙"]
+
 def crawlergo_run():
     with open('targets.txt', 'r', encoding='utf-8') as f:
         for line in f:
-            print(line, end="")
+            #print(line, end="")
             line = line.replace('\n', '')
             commond = './crawlergo -t 20 -f smart --fuzz-path --output-mode json ' + line
             output = subprocess.run(commond, shell=True, capture_output=True, text=True, encoding='utf-8')
@@ -15,27 +17,37 @@ def crawlergo_run():
             #print(r)
             domain = open("./sub_domains.txt", "r", encoding='utf-8')
             for url in r:
-                flag = 0
+                domain_in_url_flag = 0
                 for sub_domain in domain.readlines():
                     sub_domain = sub_domain.replace('\n', '')
                     if sub_domain in url:
-                        flag = 1
+                        domain_in_url_flag = 1
                         break
-                if flag == 1:
-                    print(url)
-                    url_to_ez(url)
+                if domain_in_url_flag == 1:
+                    #print(url)
+                    url_is_on_live_flag = url_to_ez(url)
+                    print(url_is_on_live_flag)
+                    if url_is_on_live_flag == 1:
+                        break
                 domain.seek(0)
 
 def url_to_ez(url):
     proxies = {
-        'http': 'http://127.0.0.1:2222',
-        'https': 'http://127.0.0.1:2222',
+        'http': 'http://127.0.0.1:8080',
+        'https': 'http://127.0.0.1:8080',
     }
     # 使用代理访问URL
     try:
-        response = requests.get(url, proxies=proxies, verify=False)
-        #print(response.text)
+        print("正在请求：" + url)
+        response = requests.get(url, proxies=proxies, verify=False, timeout=10)
+        for i in black_list:
+            if i in response.text:
+                return 1
+        return 0
     except requests.exceptions.RequestException as e:
         print(f'访问 {url} 时发生错误: {e}')
+        return 1
+    except requests.exceptions.Timeout:
+        return 1
 
 crawlergo_run()
